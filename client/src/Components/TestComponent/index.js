6abb300a4f160f1e94244932fbbc810bf79eef65
Taylor <trm313@gmail.com>
2021-03-02 21:04:31 -0500

    Recommitting client folder

---
+++ client/src/Components/TestComponent/index.js
@@ -0,0 +1,11 @@
+// import React, { useState, useEffect, useReducer } from "react";
+
+const TestComponent = (props) => {
+  return (
+    <div>
+      <h1>TestComponent</h1>
+    </div>
+  );
+};
+
+export default TestComponent;
