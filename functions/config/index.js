b0bd88008c93fe917636b56e9f6dda7a3185f2ef
Taylor <trm313@gmail.com>
2021-03-18 21:23:54 -0400

    Merging functions changes from hubbub-apps

--- functions/config/index.js
+++ functions/config/index.js
@@ -8,17 +8,28 @@ const development = require("./development");
 let env = functions.config();
 // TODO: Drop all this - just export functions.get() directly, call it more normally around
 const config = {
-  SHOPIFY_SCOPES: env.shopify.scopes || "read_products,read_customers",
-  SHOPIFY_API_KEY: env.shopify.api_key || "--shopify.api_key--",
-  SHOPIFY_API_SECRET_KEY: env.shopify.api_secret_key,
-  SHOPIFY_REDIRECT_URL: env.shopify.redirect_url,
-  CLIENT_URL: env.app.client_url,
-  API_URL: env.app.api_url,
-  APP_NAME: env.app.name,
+  SHOPIFY_SCOPES: "read_products,read_customers",
+  SHOPIFY_API_KEY: "--shopify.api_key--",
+  SHOPIFY_API_SECRET_KEY: "--shopify.api_secret_key--",
+  SHOPIFY_REDIRECT_URL: "--shopify.redirect_url--",
+  CLIENT_URL: "--app.client_url--",
+  API_URL: "--app.api_url--",
+  APP_NAME: "--app.app_name--",
   APP_STORE_NAME: "AppStoreName",
   APP_SCOPE: "read_products,read_customers",
   DATABASE_NAME: "DatabaseName",
 };
+if (env.app) {
+  config.CLIENT_URL = env.app.client_url;
+  config.API_URL = env.app.api_url;
+  config.APP_NAME = env.app.name;
+}
+if (env.shopify) {
+  config.SHOPIFY_SCOPES = env.shopify.scopes || "read_products,read_customers";
+  (config.SHOPIFY_API_KEY = env.shopify.api_key || "--shopify.api_key--"),
+    (config.SHOPIFY_API_SECRET_KEY = env.shopify.api_secret_key);
+  config.SHOPIFY_REDIRECT_URL = env.shopify.redirect_url;
+}
 
 if (env !== "PRODUCTION") {
   module.exports = Object.assign({}, config, development);
